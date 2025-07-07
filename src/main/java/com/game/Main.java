c5ab70194a57e53592dec577a93e05b028578570
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-07 18:02:44 -0500

    Lectura de Archivo

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -1,7 +1,38 @@
 package com.game;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        
+        System.out.println(leerArchivo());
+
     }
+
+    public static String leerArchivo() {
+        FileReader archivo;
+        BufferedReader lector;
+        try {
+            archivo = new FileReader("src/main/resources/Welcome.txt");
+            if (archivo.ready()) { // Verificar si esta listo para ser leido
+                lector = new BufferedReader(archivo);
+                String linea;
+                StringBuilder texto = new StringBuilder();
+                while ((linea = lector.readLine()) != null) {
+                    texto.append(linea+"\n");
+                }
+                return texto.toString();
+            } else {
+                System.out.println("El archivo no esta listo para ser leido");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error al leer el archivo");
+        }
+        return null;
+    }
+
 }
+
