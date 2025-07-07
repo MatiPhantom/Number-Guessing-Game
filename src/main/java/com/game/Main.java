739d3269d7f93cd233821546d48e45ea54954c0f
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-07 18:20:44 -0500

    Impresion de Bienvenida

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -2,20 +2,48 @@ package com.game;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+    public static byte CHANCES;
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        try{
+            String texto=leerArchivo();
+            System.out.print(texto);
+        }catch(IOException e){
+            System.out.println("Error al leer el archivo");
+            System.exit(0);
+        }
+        int dificultad=scanner.nextInt();
+        String dificultT="";
+        switch(dificultad){
+            case 1:
+                CHANCES=10;
+                dificultT="Easy";
+                break;
+            case 2:
+                CHANCES=5;
+                dificultT="Medium";
+                break;
+            case 3:
+                CHANCES=3;
+                dificultT="Hard";
+                break;
+        }
+        System.out.println("Great! You have selected the "+dificultT+" difficulty level.\nLet's start the game!\n");
+        System.out.print("Enter your guess: ");
+        byte guess=scanner.nextByte();
+        
 
         
-        System.out.println(leerArchivo());
 
     }
 
-    public static String leerArchivo() {
-        FileReader archivo;
+    public static String  leerArchivo() throws IOException {
         BufferedReader lector;
-        try {
-            archivo = new FileReader("src/main/resources/Welcome.txt");
+        try(FileReader archivo = new FileReader("src/main/resources/Welcome.txt");) {
             if (archivo.ready()) { // Verificar si esta listo para ser leido
                 lector = new BufferedReader(archivo);
                 String linea;
@@ -31,7 +59,7 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Error al leer el archivo");
         }
-        return null;
+        throw new IOException();
     }
 
 }
